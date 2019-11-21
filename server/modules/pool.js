e3e15dc36467f2a041c0d69ff146697141d453cd
Korey Peterson <korey.t.peterson@gmail.com>
2019-11-21 10:01:22 -0600

    added app.delete.

---
+++ server/modules/pool.js
@@ -0,0 +1,15 @@
+const pg = require( 'pg' );
+// db stuff
+const Pool = pg.Pool;
+const pool = new Pool({
+    database: 'music_library',
+    host: 'localhost',
+    port: 5432,
+    max: 12,
+    idleTimeoutMillis: 30000
+}); // end pool
+
+pool.on('connect', ()=>{
+    console.log( 'connected to db' );
+})
+module.exports = pool;
